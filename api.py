10f72c2d79c6ff3fffe984bb45693b629f1be79a
Denis Lavrov <bahus.vel@gmail.com>
2016-01-22 13:55:33 +1300

    Message -> (Type, Payload)

--- api.py
+++ api.py
@@ -1,8 +1,6 @@
 import time
 
 __author__ = 'denislavrov'
-import random
-import struct
 
 """
 If you want to port COR-Module to another language, you must implement everything in this module in the language.
@@ -10,29 +8,24 @@ If you want to port COR-Module to another language, you must implement everythin
 """
 
 
-def id_generator():
-	# Generates random 32bit integer
-	return struct.pack(">I", random.randint(0, 2147483647))
-
-
 class CORModule:
 
-	def __init__(self, network_adapter=None, mid=None, *args, **kwargs):
-		if mid is None:
-			mid = id_generator()
-		self.mid = mid
-		self.produces = []
+	def __init__(self, network_adapter=None, *args, **kwargs):
 		self.consumes = {}
+		try:
+			if getattr(self, "moduleID") is None:
+				raise Exception("You must specify module identifier")
+		except:
+			raise Exception("You must specify module identifier")
 		if network_adapter is None:
 			from cor.comm import CallbackNetworkAdapter
-			network_adapter = CallbackNetworkAdapter
-		self.network_adapter = network_adapter(self, **kwargs)
-		print("Initializaing %s %s" % (type(self).__name__, self.mid))
+			self.network_adapter = CallbackNetworkAdapter(self)
+		else:
+			self.network_adapter = network_adapter
+		print("Initializaing %s" % type(self).__name__)
 
 	def add_topics(self, topics):
 		self.consumes.update(topics)
-		ta = Message("TOPIC_ADVERTISEMENT", {"consumes": list(topics.keys())})
-		self.messageout(ta)
 
 	def messagein(self, message):
 		if message.atype in self.consumes:
@@ -41,37 +34,21 @@ class CORModule:
 	def messageout(self, message, sync=False, timeout=None):
 		while self.network_adapter is None:
 			time.sleep(0.01)
-		message.source.append(self.mid)
 		self.network_adapter.message_out(message)
+
+
+class Type:
+	def __init__(self, name, subtypes=None):
+		super().__init__()
+		self.name = name
+		self.extends = subtypes
 
 
 class Message:
 	def __init__(self, atype, payload):
 		super().__init__()
-		self.source = []
-		self.destination = []
 		self.atype = atype
-		self.number = random.SystemRandom().randint(0, 2147483647)
 		self.payload = payload
 
-	def src_ipid(self):
-		return b''.join(self.source)
-
-	def dst_ipid(self):
-		return b''.join(self.destination)
-
-	def src_from(self, ipid):
-		tmp = []
-		for i in range(0, len(ipid), 8):
-			tmp.append(ipid[i:i+8])
-		self.source = tmp
-
-	def dst_from(self, ipid):
-		tmp = []
-		for i in range(0, len(ipid), 8):
-			tmp.append(ipid[i:i+8])
-		self.destination = tmp
-
 	def __str__(self, *args, **kwargs):
-		return "Type: %s, Source: %s, Destination: %s, Message Number: %s, Payload: %s" % (
-			self.atype, self.source, self.destination, self.number, self.payload)
+		return "Type: %s, Payload: %s" % (self.atype, self.payload)
--- comm.py
+++ comm.py
@@ -1,198 +1,43 @@
 __author__ = 'denis'
 
-import socket
-import os
-import msgpack
-import threading
-import time
-import cor.api
-
-
-def dst_router_factory(default_route):
-	def dst_router(message):
-		if len(message.destination) == 0:
-			return [default_route]
-		else:
-			return [message.destination[-1]]
-	return dst_router
-
-
-def static_router_factory(routes):
-	def static_route(message):
-		if message.atype in routes:
-			return [routes[message.atype].mid]
-		else:
-			return [None]
-	return static_route
-
 
 class NetworkAdapter:
-
-	def poller_worker(self):
-		pass
 
 	def message_out(self, message):
 		pass
 
 	def start(self):
-		self.pollert.start()
+		pass
 
-	def __init__(self, module, route_callback=dst_router_factory("manager")):
+	def __init__(self, module):
 		super().__init__()
 		self.module = module
 		self.message_callback = module.messagein
-		self.route_callback = route_callback
-		self.pollert = threading.Thread(target=self.poller_worker)
 
 
 class CallbackNetworkAdapter(NetworkAdapter):
 
-	callback_autotable = {}
+	def handle(self, message):
+		self.message_callback(message)
 
 	def message_out(self, message):
 		super().message_out(message)
-		dsts = self.route_callback(message)
-		for dst in dsts:
-			if dst in self.callbacks:
-				self.callbacks[dst](message)
-			elif dst in CallbackNetworkAdapter.callback_autotable:
-				CallbackNetworkAdapter.callback_autotable[dst](message)
+		if message.atype in self.callbacks:
+			for callback in self.callbacks[message.atype]:
+				callback(message)
+
+	def register_callback(self, atype, modules):
+		if type(modules) is not list:
+			modules = [modules]
+		for module in modules:
+			if type(module.network_adapter) == CallbackNetworkAdapter:
+				if atype in self.callbacks:
+					self.callbacks[atype].append(module.network_adapter.handle)
+				else:
+					self.callbacks[atype] = [module.network_adapter.handle]
 			else:
-				print("Did not deliver %s, destination %s is unknown" % (str(message), str(dst)))
+				raise Exception(str(module) + " does not use CallbackNetworkAdapter, cannot register callback!")
 
-	def register_callback(self, module):
-		self.callbacks[module.mid] = module.messagein
-
-	def __init__(self, module, route_callback=dst_router_factory("manager")):
-		super().__init__(module, route_callback)
-		self.callbacks = {}
-		CallbackNetworkAdapter.callback_autotable[module.mid] = module.messagein
-
-
-class SocketModuleNetworkAdapter(NetworkAdapter):
-
-	def __init__(self, module, url):
+	def __init__(self, module):
 		super().__init__(module)
-		self.url = url
-		self.connected = False
-		self.socket = self.createSocket(url)
-
-	def createSocket(self, url):
-		parts = str(url).split("://")
-		if parts[0] == "unixsock":
-			family = socket.AF_UNIX
-			type = socket.SOCK_STREAM
-		elif parts[0] == "tcp":
-			family = socket.AF_INET
-			type = socket.SOCK_STREAM
-		else:
-			raise ValueError("UNSUPPORTED SOCKET TYPE " + url)
-
-		sock = socket.socket(family, type)
-		return sock
-
-	def connect_to_manager(self, managerif):
-		parts = str(managerif).split("://")
-		aparts = parts[1].split(":")
-		if len(aparts) == 2:
-			self.socket.connect((aparts[0], int(aparts[1])))
-		elif len(aparts) == 1:
-			self.socket.connect(aparts[0])
-		self.connected = True
-
-	def message_out(self, message):
-		super().message_out(message)
-		if not self.connected:
-			print("Dropping Message, Socket not connected")
-			return
-
-		self.socket.send(msgpack.packb(message.__dict__, use_bin_type=True))
-
-	def poller_worker(self):
-		super().poller_worker()
-		unpacker = msgpack.Unpacker(encoding="UTF-8")
-		while True:
-			if not self.connected:
-				time.sleep(0.0001)
-				continue
-			buf = self.socket.recv(4096)
-			if not buf:
-				time.sleep(0.0001)
-				continue
-			unpacker.feed(buf)
-			for unpacked in unpacker:
-				msg = cor.api.Message(None, None)
-				msg.__dict__.update(unpacked)
-				self.message_callback(msg)
-
-
-class SocketManagerNetworkAdapter(NetworkAdapter):
-
-	@staticmethod
-	def ipid(addrlist):
-		ipid = b""
-		for addr in addrlist:
-			ipid += addr
-		return ipid
-
-	def client_thread(self, conn):
-
-		unpacker = msgpack.Unpacker(encoding="UTF-8")
-		while True:
-			buf = conn.recv(4096)
-			if not buf:
-				time.sleep(0.0001)
-				continue
-			unpacker.feed(buf)
-			for unpacked in unpacker:
-				msg = cor.api.Message(None, None)
-				msg.__dict__.update(unpacked)
-				self.clients[SocketManagerNetworkAdapter.ipid(msg.source)] = conn
-				#print("Manager Received: " + str(msg))
-				self.message_callback(msg)
-
-	@staticmethod
-	def createSocket(url):
-		parts = str(url).split("://")
-		if parts[0] == "unixsock":
-			family = socket.AF_UNIX
-			type = socket.SOCK_STREAM
-			addr = parts[1]
-			try:
-				os.unlink(addr)
-			except:
-				pass
-		elif parts[0] == "tcp":
-			family = socket.AF_INET
-			type = socket.SOCK_STREAM
-			aparts = parts[1].split(":")
-			addr = (aparts[0], int(aparts[1]))
-		else:
-			raise ValueError("UNSUPPORTED SOCKET TYPE " + url)
-
-		sock = socket.socket(family, type)
-		sock.bind(addr)
-		return sock
-
-	def __init__(self, message_callback, url):
-		super().__init__(message_callback)
-		self.sock = SocketManagerNetworkAdapter.createSocket(url)
-		self.clients = {}
-
-	def poller_worker(self):
-		super().poller_worker()
-		self.sock.listen(10)
-		while True:
-			conn, addr = self.sock.accept()
-			clientt = threading.Thread(target=self.client_thread, args=(conn,))
-			clientt.start()
-
-	def message_out(self, message):
-		super().message_out(message)
-		try:
-			sock = self.clients[SocketManagerNetworkAdapter.ipid(message.destination)]
-			#print("Manager Sent: " + str(message))
-			sock.send(msgpack.packb(message.__dict__, use_bin_type=True))
-		except KeyError:
-			print("Could not find recipient for the message")
-
+		self.callbacks = {}
